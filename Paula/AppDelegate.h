4bf680ac4c66c34e8c8418baaa14235f2edd3d0a
Grant Damron <grant@inomadmusic.com>
2012-09-27 15:54:49 -0400

    initial commit

---
+++ Paula/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Paula
+//
+//  Created by Grant Damron on 9/27/12.
+//  Copyright (c) 2012 Grant Damron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Paula/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Paula
+//
+//  Created by Grant Damron on 9/27/12.
+//  Copyright (c) 2012 Grant Damron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
